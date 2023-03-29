545a5f1a50725946e585b6e3ade86137a9301d34
Hazique Quazi <hazique.q@gmail.com>
2023-03-29 17:50:58 -0600

    Correctly configured horizontal timeline

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,39 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-timeline direction="horizontal" align="start" truncate-line="end">
+    <v-timeline-item>
+      <div>
+        <div class="text-h6">Content title</div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+    </v-timeline-item>
+
+    <v-timeline-item>
+      <div>
+        <div class="text-h6">Content title</div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+    </v-timeline-item>
+
+    <v-timeline-item>
+      <div>
+        <div class="text-h6">Content title</div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+        </p>
+      </div>
+    </v-timeline-item>
+  </v-timeline>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style>
+.v-timeline-divider{
+  width: 120%;
 }
 </style>
+
